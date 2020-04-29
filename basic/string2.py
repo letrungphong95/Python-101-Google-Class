f308fa60b24fbf815debde3081533c2baec93c45
Ann Nguyen <qapril2040@gmail.com>
2020-04-29 20:49:32 +0700

    Modified 3 files

--- basic/string2.py
+++ basic/string2.py
@@ -16,8 +16,13 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
-  # +++your code here+++
-  return
+  if len(s) >= 3 and s[-3:] != 'ing':
+    return s + "ing"
+  elif len(s) >= 3 and s[-3:] == 'ing':
+    return s + 'ly'
+  else:
+    return s
+  
 
 
 # E. not_bad
@@ -29,9 +34,11 @@ def verbing(s):
 # So 'This dinner is not that bad!' yields:
 # This dinner is good!
 def not_bad(s):
-  # +++your code here+++
-  return
-
+  a = s.find('not')
+  b = s.find('bad')
+  if a != -1 and b != -1 and b > a:
+    s = s[:a] + 'good' + s[(b+3):]
+  return s
 
 # F. front_back
 # Consider dividing a string into two halves.
@@ -41,8 +48,19 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-  # +++your code here+++
-  return
+  if len(a) % 2 == 0:
+    a_front = a[:(len(a) // 2)]
+    a_back = a[(len(a) // 2):]
+  else:
+    a_front = a[:((len(a) // 2)+1)]
+    a_back = a[((len(a) // 2)+1):]
+  if len(b) % 2 == 0:
+    b_front = b[:(len(b) // 2)]
+    b_back = b[(len(b) // 2):]
+  else:
+    b_front = b[:((len(b) // 2)+1)]
+    b_back = b[((len(b) // 2)+1):]
+  return a_front + b_front + a_back + b_back
 
 
 # Simple provided test() function used in main() to print
@@ -52,26 +70,26 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # main() calls the above functions with interesting inputs,
 # using the above test() to check if the result is correct or not.
 def main():
-  print 'verbing'
+  print ('verbing')
   test(verbing('hail'), 'hailing')
   test(verbing('swiming'), 'swimingly')
   test(verbing('do'), 'do')
 
-  print
-  print 'not_bad'
+  
+  print ('not_bad')
   test(not_bad('This movie is not so bad'), 'This movie is good')
   test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
   test(not_bad('This tea is not hot'), 'This tea is not hot')
   test(not_bad("It's bad yet not"), "It's bad yet not")
 
-  print
-  print 'front_back'
+  
+  print ('front_back')
   test(front_back('abcd', 'xy'), 'abxcdy')
   test(front_back('abcde', 'xyz'), 'abcxydez')
   test(front_back('Kitten', 'Donut'), 'KitDontenut')
